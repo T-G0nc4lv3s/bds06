1fe488a6cff88a43c8508a2ac019a9635b37a88b
Thiago Gonçalves <mg.az.ul@hotmail.com>
2022-09-09 21:35:54 -0300

    Find movie by id

---
+++ src/main/java/com/devsuperior/movieflix/services/MovieService.java
@@ -0,0 +1,28 @@
+package com.devsuperior.movieflix.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.movieflix.dto.GenreDTO;
+import com.devsuperior.movieflix.dto.MovieDTO;
+import com.devsuperior.movieflix.entities.Movie;
+import com.devsuperior.movieflix.repositories.MovieRepository;
+
+
+@Service
+public class MovieService {
+
+	@Autowired
+	private MovieRepository repository;
+	
+	@Transactional(readOnly = true)
+	public MovieDTO findById(Long id) {
+		Optional<Movie> obj = repository.findById(id);
+		Movie movie = obj.orElseThrow(() -> new RuntimeException("Resource not found"));
+		GenreDTO genre = new GenreDTO(movie.getGenre());
+		return new MovieDTO(movie, genre);
+	}
+}
